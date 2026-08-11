f9c942defddb2d120facfab8e093b76be1ec09a2
ah-yis <ahyis+personal@proton.me>
2026-08-11 17:47:54 +0300

    why do they gotta make python irritating

--- startup.sh
+++ startup.sh
@@ -8,9 +8,31 @@
 # actually python might not be necessary, since i get break system package errors
 # instead, use pyenv
 
-bash ./pyenv/bin/pip install -r requirements.txt
-curl -LsSf https://astral.sh/uv/install.sh | sh
-export PATH="$HOME/.local/bin:$PATH"
+# --- check for python
+if command -v python3 &> /dev/null
+then
+    echo "Python is already installed."
+else
+    echo "Installing Python..."
+    apt install python3 python3-venv -y
+    pip install -r requirements.txt
+fi
+
+# --- check for py-dependencies
+if command -v uv &> /dev/null
+then
+    echo "uv is already installed."
+    echo "Installing Python dependencies..."
+    bash ./pyenv/bin/pip install -r requirements.txt
+else
+    echo "Installing uv..."
+    curl -LsSf https://astral.sh/uv/install.sh | sh
+    export PATH="$HOME/.local/bin:$PATH"
+    echo "Installing Python dependencies..."
+    bash ./pyenv/bin/pip install -r requirements.txt
+fi
+
+
 
 # --- check if script is run via systemd services
 #       ... if not, ask the user if they want to make a systemd service to run on startup
